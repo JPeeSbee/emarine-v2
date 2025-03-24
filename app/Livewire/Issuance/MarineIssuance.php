b6f6fd70f489ea1c59d85b656ac314d0f202b494
John Patrick S. Bendana <JPBendana@MAA>
2025-03-24 10:28:22 +0800

    initial commit ---base layout

---
+++ app/Livewire/Issuance/MarineIssuance.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Issuance;
+
+use Livewire\Component;
+
+class MarineIssuance extends Component
+{
+    public function render()
+    {
+        return view('livewire.issuance.marine-issuance');
+    }
+}
